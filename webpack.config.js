aff54d89c197701355c46a60990039387dfcab97
Andrii <kulchaka@gmail.com>
2020-10-30 14:47:42 +0100

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 const path = require('path');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -8,7 +14,26 @@ module.exports = {
         filename: 'bundle.[hash].js',
         path: path.resolve(__dirname, 'dist')
     },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@core': path.resolve(__dirname, 'src/core')
+        }
+    },
     plugins: [
-
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        new CopyPlugin({
+            patterns: [{
+                from: path.resolve(__dirname, 'src/favicon.ico'),
+                to: path.resolve(__dirname, 'dist')
+            }],
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.[hash].css'
+        })
     ]
 }
