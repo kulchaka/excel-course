fe09e7af3a0028da83f7fc752e5dbf61bc9db990
Andrii <kulchaka@gmail.com>
2020-10-30 15:35:45 +0100

    add filename

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,14 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const isProd = process.env.NODE_ENV === 'production';
 const isDev = !isProd;
 
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
+
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: './index.js',
     output: {
-        filename: 'bundle.[hash].js',
+        filename: filename('js'),
         path: path.resolve(__dirname, 'dist')
     },
     resolve: {
@@ -27,7 +29,11 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: 'index.html'
+            template: 'index.html',
+            minify: {
+                removeComments: isProd,
+                collapseWhitespace: isDev
+            }
         }),
         new CopyPlugin({
             patterns: [{
@@ -36,7 +42,7 @@ module.exports = {
             }],
         }),
         new MiniCssExtractPlugin({
-            filename: 'bundle.[hash].css'
+            filename: filename('css')
         }),
     ],
     module: {
